1dd640e04999d861ece37c82e2d2253d019bbfb0
Jiaxin.Zhang <jiaxin.zhang@datayes.com>
2019-07-23 18:52:35 +0800

    add batch

--- cifar10/data_process.py
+++ cifar10/data_process.py
@@ -7,25 +7,31 @@ import pickle
 def save_batch(data_path, batch_size, data_size=300000):
     id_list = np.arange(1, data_size+1).tolist()
     batch_num = 0
-    batch_img = None
     for i in range(0, data_size, batch_size):
+        batch_img_dict = {}
+        if batch_num in []:
+            batch_num += 1
+            continue
         batch_list = id_list[i: min(i + batch_size, data_size)]
         for j in batch_list:
             img = image.imread(data_path + '%s.png' % j)
-            img = img.reshape(1, 3, 32, 32)
-            if batch_img is not None:
-                batch_img = nd.concatenate([batch_img, img])
-            else:
-                batch_img = img
-        batch_img = batch_img.asnumpy()
+            img = img.asnumpy()
+            batch_img_dict[j] = img
+
         print('start saving batch_%s' % batch_num)
-        pickle.dump(batch_img, 'batch_%s.pkl' % batch_num)
+        pickle.dump(batch_img_dict, open('batch_%s.pkl' % batch_num, 'wb'))
         print('complete saving batch_%s' % batch_num)
-        batch_img = None
         gc.collect()
         batch_num += 1
 
-batch_size = 10000
-data_path = '/home/casey/test/'
-data_size = 300000
-save_batch(data_path, batch_size, data_size)
+def load_batch():
+    for i in range(10):
+        batch_img_dict = pickle.load(open('batch_%s.pkl' % i, 'rb'))
+
+
+
+# batch_size = 30000
+# data_path = '/home/casey/test/'
+# data_size = 300000
+# save_batch(data_path, batch_size, data_size)
+load_batch()
