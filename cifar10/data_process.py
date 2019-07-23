8cd7fa16d7af7dc8c65060fe7fc828e954176bf6
Jiaxin.Zhang <jiaxin.zhang@datayes.com>
2019-07-23 15:01:57 +0800

    add data process

---
+++ cifar10/data_process.py
@@ -0,0 +1,29 @@
+import numpy as np
+import mxnet as mx
+from mxnet import nd, image
+import gc
+
+def save_batch(data_path, batch_size, data_size=300000):
+    id_list = np.arange(1, data_size+1).tolist()
+    batch_num = 0
+    for i in range(0, data_size, batch_size):
+        batch_list = id_list[i: min(i + batch_size, data_size)]
+        batch_img = None
+        for j in batch_list:
+            img = image.imread(data_path + '%s.png' % j)
+            img = img.reshape(1, 3, 32, 32)
+            if batch_img is not None:
+                batch_img = nd.concatenate([batch_img, img])
+            else:
+                batch_img = img
+        print('start saving batch_%s' % batch_num)
+        nd.save('batch_%s' % i, batch_img)
+        print('complete saving batch_%s' % batch_num)
+        del batch_img
+        gc.collect()
+        batch_num += 1
+
+batch_size = 3000
+data_path = '/home/casey/test/'
+data_size = 300000
+save_batch(data_path, batch_size, data_size)
